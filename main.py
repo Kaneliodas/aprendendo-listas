85ff95630165fb3b85f7621d25dd8fefc738fe3d
João Vitor Sousa Ramos <79048652+Kaneliodas@users.noreply.github.com>
2024-09-29 13:37:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
 magicians = ['alice', 'david', 'carolina']
-for a in magicians:  print(a.title() + " esse foi um bom truque.")
+for a in magicians:  print(a.title() + " esse foi um bom truque." + " Mal posso esperar pelo próximo truque" + "\n" + a.title() )
